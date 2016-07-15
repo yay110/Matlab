a6db3751058c4e491a8433db313d08ef3e30828d
Zhengyi Yang <zy6@st-andrews.ac.uk>
2016-07-15 11:18:03 +0100

    complete the tek.m
    this class is about to finish, only thing that needs is to change the
    relative phase in burst mode

--- tools/Tek.m
+++ tools/Tek.m
@@ -1,4 +1,4 @@
-classdef Tek
+classdef Tek < handle
     
     % FunGenTek class is designed to control the Tektronix AFG1000 seris
     % function generator. AFG1022 is the model we have.
@@ -9,14 +9,14 @@ classdef Tek
     
     properties
         target;
-        output;
-        mode = 1;
+        output = 'On';
+        mode = 'Sin';
         frequency = 10;
-        amplitude = 0;      %Unit V;
-        offset    = 0;      %Unit V;
-        phaseOffset = 90;   %Unit degree
-        
-        
+        amplitude = 0.073;      %Unit V;
+        offset    = 0.106;      %Unit V;
+        phaseOffset = 90;   %Unit deg
+        burstCycles = 2;
+        %     burst = 'off';
     end
     
     methods
@@ -35,6 +35,7 @@ classdef Tek
             % Connect to instrument object, obj1.
             fopen(Tek.target);
             
+            Tek.mode = 1;
         end
         
         % basic function such as choosing mode, change frequency, amplitude
@@ -43,38 +44,108 @@ classdef Tek
             switch n
                 case 1
                     fprintf(Tek.target, 'Source1:FUNCTION:SHAPE SIN');
+                    Tek.mode = 'Sin';
                 case 2
                     fprintf(Tek.target, 'Source1:FUNCTION:SHAPE SQUARE');
+                    Tek.mode = 'Squre';
                 case 3
                     fprintf(Tek.target, 'Source1:FUNCTION:SHAPE Ramp');
+                    Tek.mode = 'Ramp';
                 case 4
                     fprintf(Tek.target, 'Source1:FUNCTION:SHAPE Pulse');
+                    Tek.mode = 'Pulse';
             end
         end
         
         function Tek = set.frequency(Tek,n)
             fprintf(Tek.target, ['SOURCE1:FREQUENCY ', num2str(n), 'HZ']);
+            Tek.frequency = n;
         end
         
         function Tek = set.amplitude(Tek,n)
             fprintf(Tek.target,['SOURCE1:VOLTAGE:AMPLITUDE ',num2str(n),'VPP']);
+            Tek.amplitude = n;
         end
         
         function Tek = set.offset(Tek,n)
             fprintf(Tek.target,['SOURCE1:VOLTAGE:OFFSET ',num2str(n),'V']);
+            Tek.offset = n;
         end
         
         % Output status of source1
         function Tek = outputOn(Tek)
             fprintf(Tek.target, 'OUTPUT1:STATE ON');
+            Tek.output = 'On';
         end
         
         function Tek = outputOff(Tek)
             fprintf(Tek.target, 'OUTPUT1:STATE OFF');
+            Tek.output = 'Off';
         end
         
         %Burst mode related function
+        function Tek = burstOn(Tek)
+            fprintf(Tek.target, 'SOURCE1:BURST:STATE ON');
+            %          Tek.burst = 'On';
+        end
         
+        function Tek = burstOff(Tek)
+            fprintf(Tek.target, 'SOURCE1:BURST:STATE OFF');
+            %          Tek.burst = 'Off';
+        end
+        
+        function Tek = set.burstCycles(Tek,n)
+            fprintf(Tek.target, ['SOURCE1:BURST:ncycles ',num2str(n)]);
+            Tek.burstCycles = n;
+        end
+        
+        %important function needed!!!!!
+        % change the relative phase for the burst mode
+        % this function is not added because there is no mention of it on
+        % the programmer manual.
+        % need to contact the company to add it. 
+        
+        function Tek = scan(Tek,n)
+            Tek.burstOn;
+            Tek.frequency = n;
+            Tek.burstCycles = 1;
+            switch n
+                case 10
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                    Tek.burstCycles = 50;
+                case 20
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 25
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 30
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 50
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 80
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 200
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 250
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 300
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                case 320
+                    Tek.offset = 0.106;                    
+                    Tek.amplitude = 0.073;
+                otherwise
+                    disp('This frequency is not calibrated!');
+            end
+            Tek.outputOn;
+        end
         
     end
     
