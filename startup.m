428a873873eb442903ff13fb63bbbc608bf5aa5d
Zhengyi Yang <yangzy.uk@gmail.com>
2017-04-11 11:38:38 +0000

    adding ignore file

--- startup.m
+++
@@ -1,8 +0,0 @@
-addpath (genpath('C:\Users\yay-1_000\Documents\MATLAB'));
-addpath (genpath('C:\Users\yay-1_000\Documents\GitHub'));
-addpath (genpath('Z:\Matlab'));
-addpath (genpath('C:\Users\yay-1_000\Documents\matlab from st andrews'));
-addpath (genpath('C:\Users\Zhengyi\Documents\MATLAB'));
-addpath (genpath('C:\Users\Zhengyi\Documents\GitHub'));
-addpath (genpath('C:\Users\Zhengyi\Documents\software\matlab'));
-
---
+++ timeSeqTemMeasureWithArduino.m
@@ -0,0 +1,29 @@
+%connect arduino
+clear;
+
+board = Arduino();
+board.connect;
+
+time = 1200;
+temperature(1:time,1:2) = 0;
+
+for t = 1:time
+    t
+    tic
+    temperature(t,:) = board.temperature;
+% temperature(t,:)
+timeSpend = toc
+    pause(0-toc);
+end
+
+plot(temperature(:,1),'r');
+hold on;
+plot(temperature(:,2),'b');
+% plot(temperature(:,3),'g');
+% plot(temperature(:,4),'k');
+% plot(temperature(:,5),'c');
+hold off
+
+ylim([20 70])
+
+board.disconnect;
