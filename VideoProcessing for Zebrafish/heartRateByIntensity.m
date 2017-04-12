325843abb5ad9a89e1ee22d76d542fdf5787ff90
Zhengyi Yang <yangzy.uk@gmail.com>
2017-04-12 21:35:22 +0000

    extract heart rate
    so multiple folders can be done.
    generate beating better

--- VideoProcessing for Zebrafish/heartRateByIntensity.m
+++ VideoProcessing for Zebrafish/heartRateByIntensity.m
@@ -1,26 +1,26 @@
+
+clear
 %% =================process Images =================
-clear;
+dataFolder(1).name = 'E:\2017-03-29 Zebrafish heartbeat\6th sample 2Vpp 1.457Mhz\1. before drug';
+dataFolder(2).name = 'E:\2017-03-29 Zebrafish heartbeat\6th sample 2Vpp 1.457Mhz\2.adding and after drug';
+dataFolder(3).name = 'E:\2017-03-29 Zebrafish heartbeat\6th sample 2Vpp 1.457Mhz\3.washing away drug';
+dataFolder(4).name = 'E:\2017-03-29 Zebrafish heartbeat\7th sample\1. before drug';
+dataFolder(5).name = 'E:\2017-03-29 Zebrafish heartbeat\7th sample\2. adding to 320mgL';
+dataFolder(6).name = 'E:\2017-03-29 Zebrafish heartbeat\7th sample\3. adding to 500ml';
+dataFolder(7).name = 'E:\2017-03-29 Zebrafish heartbeat\7th sample\4. washing away';
+dataFolder(8).name = 'E:\2017-03-29 Zebrafish heartbeat\8th sample\1.before drug';
+dataFolder(9).name = 'E:\2017-03-29 Zebrafish heartbeat\8th sample\2. adding to 320mgL';
+dataFolder(10).name = 'E:\2017-03-29 Zebrafish heartbeat\8th sample\3. adding to 500mgL';
+dataFolder(11).name = 'E:\2017-03-29 Zebrafish heartbeat\8th sample\4. washing away';
 
-dataFolder =["E:\2017-03-29 Zebrafish heartbeat\6th sample 2Vpp 1.457Mhz\1. before drug";...
-    ''E:\2017-03-29 Zebrafish heartbeat\6th sample 2Vpp 1.457Mhz\2.adding and after drug'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\6th sample 2Vpp 1.457Mhz\3.washing away drug'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\7th sample\1. before drug'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\7th sample\2. adding to 320mgL'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\7th sample\3. adding to 500ml'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\7th sample\4. washing away'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\8th sample\1.before drug'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\8th sample\2. adding to 320mgL'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\8th sample\3. adding to 500mgL'';...
-    ''E:\2017-03-29 Zebrafish heartbeat\8th sample\4. washing away''];
-
-for folder = 1:%length(dataFolder)
+for folder = 4:length(dataFolder)
+    clear profile;
+    folderName = dataFolder(folder).name%folder);
     
-    folderName = dataFolder(10)%folder);
-    
-    folderNames = dir(string(folderName));
+    folderNames = dir(folderName);
     
     if folderNames(3).isdir
-        for n = 1:length(folderNames)-2
+        for n = 1:length(folderNames)-4
             subFolderNames(n).name = strcat(folderName,'\',folderNames(n+2).name);
         end
         sectionLength = 9;
@@ -29,7 +29,6 @@ for folder = 1:%length(dataFolder)
         sectionLength = 9;
     end
     
-%     tic
     for nn = 1:length(subFolderNames)
         cd(subFolderNames(1).name)
         fileName = dir('**.tif*');
@@ -40,22 +39,20 @@ for folder = 1:%length(dataFolder)
         
         sectionNumber = floor(length(fileName)/(frameNumber));
         
-        % read the first frame
-        firstFrame = imread(strcat(subFolderNames(nn).name,'\',fileName(2).name));
-        % find maxima on the frame
-        [x y] = find(firstFrame ==max(firstFrame(:)));
-        
-        
         for n=1:sectionNumber
+            
+            % read the first frame
+            firstFrame = imread(strcat(subFolderNames(nn).name,'\',fileName((n-1)*frameNumber+1).name));
+            % find maxima on the frame
+            [x y] = find(firstFrame ==max(firstFrame(:)));
+            
             for m = 1:frameNumber
                 currentFrame = imread(strcat(subFolderNames(nn).name,'\',fileName((n-1)*frameNumber+m).name));
                 profile(nn,n,m) = sum(sum(currentFrame(x-50:x,y-50:y)));
             end
+            
         end
         
-        
-%         t = toc;
-%         remainingTime = t/nn * (length(subFolderNames)-nn)/60
     end
     
     profile = squeeze(profile);
